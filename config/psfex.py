1821397eae82cab13dbce00890488348041cf3bb
Russell Owen <rowen@uw.edu>
2016-03-31 09:54:41 -0700

    Update config override file for new star selectors
    Star selectors are no longer in a registry, so a few tweaks
    were needed to a config override file (though I'm puzzled
    why a config override file is in this package).

--- config/psfex.py
+++ config/psfex.py
@@ -1,17 +1,16 @@
 import sys
-import lsst.meas.extensions.psfex.psfexStarSelector
-import lsst.meas.extensions.psfex.psfexPsfDeterminer
+from lsst.meas.extensions.psfex import PsfExStarSelectorTask
 
-config.calibrate.measurePsf.starSelector.name  = "psfex" if False else "objectSize"
+config.calibrate.measurePsf.starSelector.retarget(PsfExStarSelectorTask)
 config.calibrate.measurePsf.psfDeterminer.name = "psfex" if True  else "pca"
 
-config.calibrate.measurePsf.starSelector["objectSize"].sourceFluxField = "initial.flux.psf"
+config.calibrate.measurePsf.starSelector.sourceFluxField = "initial.flux.psf"
 try:
-    config.calibrate.measurePsf.starSelector["objectSize"].nSigmaClip = 1.75
+    config.calibrate.measurePsf.starSelector.nSigmaClip = 1.75
 except AttributeError, e:
     print >> sys.stderr, "Warning: %s" % (e,) # needs a post-June-2013 meas_algorithms
 
-config.calibrate.measurePsf.starSelector["psfex"].maxFwhmVariability = 0.1
-config.calibrate.measurePsf.starSelector["psfex"].maxbadflag = False
+config.calibrate.measurePsf.starSelector.maxFwhmVariability = 0.1
+config.calibrate.measurePsf.starSelector.maxbadflag = False
 
 config.calibrate.measurePsf.psfDeterminer["psfex"].spatialOrder = 1 if False else 2
